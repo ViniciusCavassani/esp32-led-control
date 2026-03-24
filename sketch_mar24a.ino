178828c3f572118d50758894121bd61203c91fd3
Cavassani <102699747+ViniciusCavassani@users.noreply.github.com>
2026-03-24 08:28:25 -0300

    Add files via upload

---
+++ sketch_mar24a.ino
@@ -0,0 +1,21 @@
+int led=2;
+int botao1=5;
+int botao2=4;
+
+void setup() 
+{
+  pinMode(led, OUTPUT);
+  pinMode(botao1, INPUT);
+  pinMode(botao2, INPUT);
+}
+void loop() 
+{
+ if (digitalRead(botao1)==1)
+{
+digitalWrite(led, HIGH);
+}
+if (digitalRead(botao2)==1)
+{
+digitalWrite(led, LOW);
+}
+}
